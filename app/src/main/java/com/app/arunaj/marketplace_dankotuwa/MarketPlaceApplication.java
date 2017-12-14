feecc91052b4b340eb34d4abd7f5ade075a1a78e
arunadj <arunadilshanjayathilake@yahoo.com>
2017-12-14 12:10:04 +0530

    initial commit

---
+++ app/src/main/java/com/app/arunaj/marketplace_dankotuwa/MarketPlaceApplication.java
@@ -0,0 +1,10 @@
+package com.app.arunaj.marketplace_dankotuwa;
+
+import android.app.Application;
+
+/**
+ * Created by arunaj on 12/14/17.
+ */
+
+public class MarketPlaceApplication extends Application {
+}
---
+++ app/src/main/java/com/app/arunaj/marketplace_dankotuwa/presentation/feature/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.arunaj.marketplace_dankotuwa.presentation.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.arunaj.marketplace_dankotuwa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
